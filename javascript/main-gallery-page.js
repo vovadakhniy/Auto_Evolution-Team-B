5e8f2cc5520b095aff82441049a78be4c3068363
Vova Dakhniy <vova.dakhniy@gmail.com>
2017-04-22 20:03:51 +0300

    Added the final version of the home-page

---
+++ javascript/main-gallery-page.js
@@ -0,0 +1,5 @@
+ $(function(){
+            $('.mg-space-init').mgSpace();
+        });
+
+ 
---
+++ javascript/main-gallery-page_breakpoint.js
@@ -0,0 +1,22 @@
+$('.mg-space-init').mgSpace({
+  // Breakpoints at which the accordian changes # of columns
+  breakpointColumns: [
+      {
+          breakpoint: 0,
+          column: 1
+      },
+      {
+          breakpoint: 568,
+          column: 2
+      },
+      {
+          breakpoint: 768,
+          column: 3
+      },
+      {
+          breakpoint: 1200,
+          column: 4
+      }
+  ],
+
+  
